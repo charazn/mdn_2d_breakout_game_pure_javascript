262a88435b39560efc8144919b27d0bd1dcd53e5
Char Aznable <charazn37@gmail.com>
2016-12-10 10:15:20 +0800

    Make ball bounce off paddle

--- main.js
+++ main.js
@@ -31,180 +31,179 @@ var score = 0;
 var lives = 3;
 
 for (c = 0; c < brickColumnCount; c++) { // Column as x or horizontal
-    bricks[c] = [];
-    for (r = 0; r < brickRowCount; r++) { // Row as y or vertical
-        bricks[c][r] = {
-            x: 0,
-            y: 0,
-            status: 1
-        };
-    }
+  bricks[c] = [];
+  for (r = 0; r < brickRowCount; r++) { // Row as y or vertical
+    bricks[c][r] = {
+      x: 0,
+      y: 0,
+      status: 1
+    };
+  }
 }
 
 // To get a random floating point number within a certain range, first define the range by 'min' and 'max':
 // min + (Math.random() * (max - min))
 // See https://answers.yahoo.com/question/index?qid=20101101170531AA5KTeQ
 function random(min, max) {
-    var num = Math.floor(Math.random() * (max - min)) + min;
-    return num;
+  var num = Math.floor(Math.random() * (max - min)) + min;
+  return num;
 }
 
 function drawScore() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#0095DD";
-    ctx.fillText("Score: " + score, 8, 20);
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Score: " + score, 8, 20);
 }
 
 function drawLives() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#0095DD";
-    ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
 }
 
 function drawBall(color) {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = color; // "#0095DD"
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = color; // "#0095DD"
+  ctx.fill();
+  ctx.closePath();
 }
 
 function changeColor() {
-    ballColor = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+  ballColor = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
 }
 
 function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
 }
 
 function drawBricks() {
-    for (c = 0; c < brickColumnCount; c++) { // Drawing columns first
-        for (r = 0; r < brickRowCount; r++) { // Then the rows
-            if (bricks[c][r].status === 1) {
-                var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-                var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
-                bricks[c][r].x = brickX;
-                bricks[c][r].y = brickY;
-                ctx.beginPath();
-                ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = "#0095DD";
-                ctx.fill();
-                ctx.closePath();
-            }
-        }
+  for (c = 0; c < brickColumnCount; c++) { // Drawing columns first
+    for (r = 0; r < brickRowCount; r++) { // Then the rows
+      if (bricks[c][r].status === 1) {
+        var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+        var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
     }
+  }
 }
 
 function increaseSpeed() {
-    if (dx < 0) {
-        dx -= 0.1; // increase horizontal speed left
-    } else {
-        dx += 0.1; // increase horizontal speed right
-    }
-    dy -= 0.1; // increase the vertical speed up
+  if (dx < 0) {
+    dx -= 0.1; // increase horizontal speed left
+  } else {
+    dx += 0.1; // increase horizontal speed right
+  }
+  dy -= 0.1; // increase the vertical speed up
 }
 
 function keyDownHandler(e) {
-    if (e.keyCode === 37) {
-        leftPressed = true;
-    } else if (e.keyCode === 39) {
-        rightPressed = true;
-    }
+  if (e.keyCode === 37) {
+    leftPressed = true;
+  } else if (e.keyCode === 39) {
+    rightPressed = true;
+  }
 }
 
 function keyUpHandler(e) {
-    if (e.keyCode === 37) {
-        leftPressed = false;
-    } else if (e.keyCode === 39) {
-        rightPressed = false;
-    }
+  if (e.keyCode === 37) {
+    leftPressed = false;
+  } else if (e.keyCode === 39) {
+    rightPressed = false;
+  }
 }
 
 function mouseMoveHandler(e) {
-    var relativeX = e.clientX - canvas.offsetLeft;
-    if (relativeX > (paddleWidth / 2) && relativeX < canvas.width - (paddleWidth / 2)) {
-        paddleX = relativeX - paddleWidth / 2;
-        // setting the left corner position of the paddle, with the mouse x position at relativeX, ie. middle of the paddle
-    }
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > (paddleWidth / 2) && relativeX < canvas.width - (paddleWidth / 2)) {
+    paddleX = relativeX - paddleWidth / 2;
+    // setting the left corner position of the paddle, with the mouse x position at relativeX, ie. middle of the paddle
+  }
 }
 
 function collisionDetection() {
-    for (c = 0; c < brickColumnCount; c++) {
-        for (r = 0; r < brickRowCount; r++) {
-            var b = bricks[c][r];
-            if (b.status == 1) {
-                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
-                    dy = -dy;
-                    changeColor();
-                    b.status = 0;
-                    score++;
-                    if (score == brickRowCount * brickColumnCount) {
-                        alert("YOU WIN, CONGRATULATIONS!");
-                        document.location.reload();
-                    }
-                }
-            }
+  for (c = 0; c < brickColumnCount; c++) {
+    for (r = 0; r < brickRowCount; r++) {
+      var b = bricks[c][r];
+      if (b.status == 1) {
+        if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+          dy = -dy;
+          changeColor();
+          b.status = 0;
+          score++;
+          if (score == brickRowCount * brickColumnCount) {
+            alert("YOU WIN, CONGRATULATIONS!");
+            document.location.reload();
+          }
         }
+      }
     }
+  }
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // See https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clearRect
-    // The CanvasRenderingContext2D.clearRect() method of the Canvas 2D API sets all pixels in the rectangle defined by starting point (x, y) and size (width, height) to transparent black, erasing any previously drawn content.
-    drawBricks();
-    drawBall(ballColor);
-    drawPaddle();
-    collisionDetection();
-    drawScore();
-    drawLives();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // See https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clearRect
+  // The CanvasRenderingContext2D.clearRect() method of the Canvas 2D API sets all pixels in the rectangle defined by starting point (x, y) and size (width, height) to transparent black, erasing any previously drawn content.
+  drawBricks();
+  drawBall(ballColor);
+  drawPaddle();
+  collisionDetection();
+  drawScore();
+  drawLives();
 
-    if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) { // Left || Right
-        // If position of center of ball and the next change in horizontal movement is smaller than the radius of the ball, that means the ball has hit the left side of the canvas. This also means that the ball must have been moving leftwards, ie. dx is -2. Therefore now set dx to be positive and move it rightwards.
-        dx = -dx;
-        changeColor();
+  if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) { // Left || Right
+    // If position of center of ball and the next change in horizontal movement is smaller than the radius of the ball, that means the ball has hit the left side of the canvas. This also means that the ball must have been moving leftwards, ie. dx is -2. Therefore now set dx to be positive and move it rightwards.
+    dx = -dx;
+    changeColor();
+  }
+
+  if (y + dy < ballRadius) { // Top
+    // Likewise, if the position of the center of the ball and the change in vertical movement is greater than the height of canvas minus the ball radisu, it means that the ball has hit the bottom wall of the canvas, meaning it is moving downwards, ie. dy is positive. Therefore, now set dy to be negative and move it upwards.
+    dy = -dy;
+    changeColor();
+  } else if ((y + dy > canvas.height - paddleHeight - ballRadius) && x > paddleX && x < paddleX + paddleWidth) {
+    // Checks that the x-position of ball is within the length of the paddle, but it still hits the wall, not the paddle.
+    // Add check if ball is hitting top of paddle
+    dy = -dy;
+    increaseSpeed();
+    para.innerHTML = "Horizontal speed dx is: " + dx + "<br>Vertical speed dy is now: " + dy;
+    // Note para.textContent does not recognise escape characters \n and html tags <br>
+  } else if (y + dy > canvas.height - ballRadius) { // Bottom
+    lives--;
+    if (!lives) {
+      alert("GAME OVER");
+      document.location.reload();
+    } else {
+      x = random(canvas.width / 4, canvas.width * 3 / 4);
+      y = random(canvas.height / 2, canvas.height * 3 / 4);
+      dx = Math.random() + 1;
+      dy = Math.random() + 1;
+      paddleX = (canvas.width - paddleWidth) / 2;
     }
+  }
 
-    if (y + dy < ballRadius) { // Top
-        // Likewise, if the position of the center of the ball and the change in vertical movement is greater than the height of canvas minus the ball radisu, it means that the ball has hit the bottom wall of the canvas, meaning it is moving downwards, ie. dy is positive. Therefore, now set dy to be negative and move it upwards.
-        dy = -dy;
-        changeColor();
-    } else if (y + dy > canvas.height - ballRadius) { // Bottom
-        if (x > paddleX && x < paddleX + paddleWidth) {
-            // checks that the x-position of ball is within the length of the paddle, but it still hits the wall, not the paddle
-            dy = -dy;
-            increaseSpeed();
-            para.innerHTML = "Horizontal speed dx is: " + dx + "<br>Vertical speed dy is now: " + dy;
-            // Note para.textContent does not recognise escape characters \n and html tags <br>
-        } else {
-            lives--;
-            if (!lives) {
-                alert("GAME OVER");
-                document.location.reload();
-            } else {
-                x = random(canvas.width / 4, canvas.width * 3 / 4);
-                y = random(canvas.height / 2, canvas.height * 3 / 4);
-                dx = Math.random() + 1;
-                dy = Math.random() + 1;
-                paddleX = (canvas.width - paddleWidth) / 2;
-            }
-        }
-    }
+  x += dx;
+  y += dy;
 
-    x += dx;
-    y += dy;
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 5; // random(5, 10)
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 5;
+  }
 
-    if (rightPressed && paddleX < canvas.width - paddleWidth) {
-        paddleX += 5; // random(5, 10)
-    } else if (leftPressed && paddleX > 0) {
-        paddleX -= 5;
-    }
-
-    requestAnimationFrame(draw);
+  requestAnimationFrame(draw);
 }
 
 // For the center of the ball to be inside the brick, all four of the following statements need to be true:
